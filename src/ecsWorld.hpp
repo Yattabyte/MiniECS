78557f0b9ef6d9428d2adaa276b2855e68d4078c
Yattabyte <tklowry93@gmail.com>
2020-05-16 12:38:55 -0500

    test, don't use this

--- src/ecsWorld.hpp
+++ src/ecsWorld.hpp
@@ -140,12 +140,12 @@ class ecsWorld {
     /// \brief  Retrieve a list of entity components corresponding to the input.
     /// \param	componentTypes		list of component types to retrieve.
     template <typename... T_types>
-    std::vector<std::tuple<T_types...>> getComponents(
-        const std::vector<std::pair<ComponentID, ecsSystem::RequirementsFlag>>&
-            componentTypes) {
+    std::vector<std::tuple<T_types...>>
+    getComponents(const std::vector<
+                  std::pair<ComponentID, ecsSystem::RequirementsFlag>>&) {
         std::vector<std::tuple<T_types...>> entityComponents;
 
-        // Cast each component set to the types requested
+        /*// Cast each component set to the types requested
         for (auto& groupedComponents : getRelevantComponents(componentTypes)) {
             // Convert the component set to a standard array
             std::array<ecsBaseComponent*, sizeof...(T_types)> arr;
@@ -159,7 +159,7 @@ class ecsWorld {
                         dynamic_cast<T_types>(args)...);
                 },
                 arr);
-        }
+        }*/
         return entityComponents;
     }
 
