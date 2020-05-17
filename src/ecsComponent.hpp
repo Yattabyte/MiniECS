834b6397f0fde258c745560ea03f1162d86adbb7
Yattabyte <tklowry93@gmail.com>
2020-05-16 21:40:58 -0500

    Update ecsComponent.hpp

--- src/ecsComponent.hpp
+++ src/ecsComponent.hpp
@@ -34,7 +34,7 @@ class ecsBaseComponent {
     virtual ~ecsBaseComponent() = default;
     ///////////////////////////////////////////////////////////////////////////
     /// \brief  Default construct this base ecsComponent.
-    ecsBaseComponent() = default;
+    ecsBaseComponent() {}
     ///////////////////////////////////////////////////////////////////////////
     /// \brief  Construct a base ecsComponent.
     /// \param	ID      the runtime ID for this component.
